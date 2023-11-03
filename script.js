926f2307400953d3b6c71c3710038b35fcf0e76e
rajakhan017 <rajakhan017@gmail.com>
2023-11-03 16:24:37 +0530

    Dom-Project-1

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get a reference to the div element by its ID and store it in a variable
+const myDiv = document.getElementById("myDiv");
+//Change the color to blue
+myDiv.style.color="blue",
+// Change the background color to yellow
+myDiv.style.backgroundColor = "yellow";
+// Add a margin of 20 pixels
+myDiv.style.margin = "20px";
+// Apply a padding of 10 pixels
+myDiv.style.padding = "10px";
+// Change the font size to 18 pixels
+myDiv.style.fontSize = "18px";
+// Set the font weight to bold
+myDiv.style.fontWeight = "bold";
+// Change the height to 200 pixels
+myDiv.style.height = "200px";
+// Modify the width to 300 pixels
+myDiv.style.width = "300px";
